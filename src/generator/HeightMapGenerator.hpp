6f550ba529dda3a87f0cbad5f8d7de6a658a7cf6
lautrec93 <lautrec.willson@gmail.com>
2026-03-12 05:04:34 +0100

    fix: change vector's data type, delete extra objects

--- src/generator/HeightMapGenerator.hpp
+++ src/generator/HeightMapGenerator.hpp
@@ -1,29 +1,27 @@
 #pragma once
 
-#include "HeightMapRaw.hpp"
 #include "TerrainConfig.hpp"
 #include <vector>
 
 class HeightMapGenerator {
 private:
-  HeightMapRaw heightMapRaw;
   TerrainConfig terrainConfig;
   unsigned coordX{};
   unsigned coordY{};
   unsigned numberOfNodes{};
-  std::vector<std::vector<unsigned>> fieldXY;
-  std::vector<std::vector<unsigned>>::iterator itera{fieldXY.begin()};
+  std::vector<std::vector<double>> fieldXY;
+  std::vector<std::vector<double>>::iterator itera{fieldXY.begin()};
   double amplitude{};
   double frequency{};
 
 public:
   void fieldXYCreator();
   double fractalBrownianMotion();
-  virtual double noise() = 0;
+  const std::vector<std::vector<double>> &getFieldXY() const;
 
   HeightMapGenerator(const TerrainConfig &config, unsigned x, unsigned y,
-                     std::vector<std::vector<unsigned>> field,
-                     std::vector<std::vector<unsigned>>::iterator iterator)
+                     std::vector<std::vector<double>> field,
+                     std::vector<std::vector<double>>::iterator iterator)
       : terrainConfig(config), coordX(x), coordY(y),
         numberOfNodes{
             static_cast<unsigned>(terrainConfig.size / terrainConfig.cellSize) -
