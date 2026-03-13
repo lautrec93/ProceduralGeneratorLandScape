2668ab668e9902a491f229cf81e7a75cebde7b51
lautrec93 <lautrec.willson@gmail.com>
2026-03-13 01:10:07 +0100

    feat: add objects declaration and methods callings

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
+#pragma once
+#include "generator/HeightMapGenerator.hpp"
+#include "generator/HeightMapRaw.hpp"
+#include "generator/Noise.hpp"
+#include "generator/PerlinNoise.hpp"
+#include "generator/TerrainConfig.hpp"
 #include <iostream>
+int main() {
+  TerrainConfig terrainMainConfig;
+  terrainMainConfig.cellSize = 10;
+  terrainMainConfig.size = 100;
+  terrainMainConfig.lacunarity = 2.0;
+  terrainMainConfig.persistence = 0.5;
+  terrainMainConfig.octaveNumber = 4;
 
-int main() { std::cout << "hello" << std::endl; }
+  unsigned nodes = terrainMainConfig.size / terrainMainConfig.cellSize - 1;
+
+  HeightMapGenerator heightMainMapGenerator{terrainMainConfig};
+
+  heightMainMapGenerator.fieldXYCreator(nodes);
+
+  // PerlinNoise perlinNoise{0.01, heightMainMapGenerator};
+  // perlinNoise.noise(nodes);
+  // heightMainMapGenerator.fractalBrownianMotion(perlinNoise);
+
+  HeightMapRaw heightMainMapRaw{heightMainMapGenerator};
+  heightMainMapRaw.showRawMap(heightMainMapGenerator);
+}
