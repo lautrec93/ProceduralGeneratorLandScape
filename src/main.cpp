6d6dd196c1b500988803c0f02001d516689c5eb7
lautrec93 <lautrec.willson@gmail.com>
2026-03-16 20:23:24 +0100

    fix: smal fixies

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,28 @@
 #include "generator/HeightMapRaw.hpp"
 #include "generator/Noise.hpp"
 #include "generator/PerlinNoise.hpp"
+#include "generator/SaveJSON.hpp"
 #include "generator/TerrainConfig.hpp"
 #include <iostream>
 int main() {
   TerrainConfig terrainMainConfig;
   terrainMainConfig.cellSize = 10;
-  terrainMainConfig.size = 100;
+  terrainMainConfig.size = 1000;
   terrainMainConfig.lacunarity = 2.0;
   terrainMainConfig.persistence = 0.5;
   terrainMainConfig.octaveNumber = 4;
 
-  unsigned nodes = terrainMainConfig.size / terrainMainConfig.cellSize - 1;
+  unsigned nodes = terrainMainConfig.size / terrainMainConfig.cellSize;
 
   HeightMapGenerator heightMainMapGenerator{terrainMainConfig};
 
   heightMainMapGenerator.fieldXYCreator(nodes);
 
-  // PerlinNoise perlinNoise{0.01, heightMainMapGenerator};
-  // perlinNoise.noise(nodes);
-  // heightMainMapGenerator.fractalBrownianMotion(perlinNoise);
+  PerlinNoise perlinNoise{0.05f, heightMainMapGenerator};
+  perlinNoise.noise(nodes, heightMainMapGenerator.getFieldXY());
+  heightMainMapGenerator.fractalBrownianMotion(perlinNoise);
 
   HeightMapRaw heightMainMapRaw{heightMainMapGenerator};
   heightMainMapRaw.showRawMap(heightMainMapGenerator);
+  saveJSON(heightMainMapGenerator.getFieldXY(), "heightmap.json");
 }
