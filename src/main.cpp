675c024ad2669a7d1ff28a8f431c26c6ac83edd7
lautrec93 <lautrec.willson@gmail.com>
2026-03-18 00:14:50 +0100

    chore: necessary obect and methods declared

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #pragma once
 #include "generator/FractalBrownianMotion.hpp"
 #include "generator/HeightMapGenerator.hpp"
-#include "generator/HeightMapRaw.hpp"
+#include "generator/HeightMapRawContainer.hpp"
 #include "generator/NoiseParametres.hpp"
 #include "generator/PerlinNoise.hpp"
 #include "generator/SaveJSON.hpp"
 #include "generator/TerrainConfig.hpp"
 #include <random>
+#include <vector>
 int main() {
   TerrainConfig terrainMainConfig;
   terrainMainConfig.cellSize = 10;
   terrainMainConfig.size = 1000;
   terrainMainConfig.lacunarity = 2.0;
   terrainMainConfig.persistence = 0.5;
-  terrainMainConfig.octaveNumber = 3;
+  terrainMainConfig.octaveNumber = 4;
   std::random_device rnd;
   std::mt19937 gen(rnd());
   std::uniform_int_distribution<> limits(100000000, 999999999);
@@ -23,17 +24,18 @@ int main() {
 
   HeightMapGenerator heightMainMapGenerator{terrainMainConfig};
 
-  FractalBrownianMotion fBM{heightMainMapGenerator, terrainMainConfig};
-
   heightMainMapGenerator.fieldXYCreator(nodes);
 
   NoiseParametres perlinNoiseParam{heightMainMapGenerator.getFieldXY(),
                                    terrainMainConfig};
 
   PerlinNoise perlinNoise{0.05f, heightMainMapGenerator};
-  fBM.fractalBrownianMotion(perlinNoise);
 
-  HeightMapRaw heightMainMapRaw{heightMainMapGenerator};
-  heightMainMapRaw.showRawMap(heightMainMapGenerator);
-  saveJSON(heightMainMapGenerator.getFieldXY(), "heightmap.json");
+  FractalBrownianMotion fBM{heightMainMapGenerator, terrainMainConfig};
+  auto finalHMap = fBM.fractalBrownianMotion(perlinNoise);
+
+  HeightMapRawContainer<std::vector<std::vector<double>>> rawContainer(
+      finalHMap);
+  rawContainer.showRawMap();
+  saveJSON(finalHMap, "heightmap.json");
 }
