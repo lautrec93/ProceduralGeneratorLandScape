3f25f8fbe2bff73420d4766d0f3b1b856a4e218a
lautrec93 <lautrec.willson@gmail.com>
2026-03-17 04:52:15 +0100

    feat: add random for seed, chore: declared new classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,37 @@
 #pragma once
+#include "generator/FractalBrownianMotion.hpp"
 #include "generator/HeightMapGenerator.hpp"
 #include "generator/HeightMapRaw.hpp"
-#include "generator/Noise.hpp"
+#include "generator/NoiseParametres.hpp"
 #include "generator/PerlinNoise.hpp"
 #include "generator/SaveJSON.hpp"
 #include "generator/TerrainConfig.hpp"
-#include <iostream>
+#include <random>
 int main() {
   TerrainConfig terrainMainConfig;
   terrainMainConfig.cellSize = 10;
   terrainMainConfig.size = 1000;
   terrainMainConfig.lacunarity = 2.0;
   terrainMainConfig.persistence = 0.5;
-  terrainMainConfig.octaveNumber = 4;
+  terrainMainConfig.octaveNumber = 3;
+  std::random_device rnd;
+  std::mt19937 gen(rnd());
+  std::uniform_int_distribution<> limits(100000000, 999999999);
+  terrainMainConfig.seed = limits(gen);
 
   unsigned nodes = terrainMainConfig.size / terrainMainConfig.cellSize;
 
   HeightMapGenerator heightMainMapGenerator{terrainMainConfig};
 
+  FractalBrownianMotion fBM{heightMainMapGenerator, terrainMainConfig};
+
   heightMainMapGenerator.fieldXYCreator(nodes);
 
+  NoiseParametres perlinNoiseParam{heightMainMapGenerator.getFieldXY(),
+                                   terrainMainConfig};
+
   PerlinNoise perlinNoise{0.05f, heightMainMapGenerator};
-  perlinNoise.noise(nodes, heightMainMapGenerator.getFieldXY());
-  heightMainMapGenerator.fractalBrownianMotion(perlinNoise);
+  fBM.fractalBrownianMotion(perlinNoise);
 
   HeightMapRaw heightMainMapRaw{heightMainMapGenerator};
   heightMainMapRaw.showRawMap(heightMainMapGenerator);
