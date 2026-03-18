c9d99ec02cd4d9fdd8dc58bee9de588855dcf120
lautrec93 <lautrec.willson@gmail.com>
2026-03-19 00:14:56 +0100

    chore: reorganisation of object and functions declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "generator/FractalBrownianMotion.hpp"
+#include "generator/HeightMapFinContainer.hpp"
 #include "generator/HeightMapGenerator.hpp"
 #include "generator/HeightMapRawContainer.hpp"
 #include "generator/NoiseParametres.hpp"
 #include "generator/PerlinNoise.hpp"
+#include "generator/PostProcessor.hpp"
 #include "generator/SaveJSON.hpp"
 #include "generator/TerrainConfig.hpp"
 #include <random>
@@ -11,16 +13,20 @@
 int main() {
   TerrainConfig terrainMainConfig;
   terrainMainConfig.cellSize = 10;
-  terrainMainConfig.size = 1000;
+  terrainMainConfig.size = 40000;
   terrainMainConfig.lacunarity = 2.0;
   terrainMainConfig.persistence = 0.5;
   terrainMainConfig.octaveNumber = 4;
+  terrainMainConfig.minHeight = -1000;
+  terrainMainConfig.maxHeight = 4000;
+  terrainMainConfig.seaLevel = 0.25;
   std::random_device rnd;
   std::mt19937 gen(rnd());
   std::uniform_int_distribution<> limits(100000000, 999999999);
   terrainMainConfig.seed = limits(gen);
 
   unsigned nodes = terrainMainConfig.size / terrainMainConfig.cellSize;
+  double coefficient = 2.5;
 
   HeightMapGenerator heightMainMapGenerator{terrainMainConfig};
 
@@ -36,6 +42,16 @@ int main() {
 
   HeightMapRawContainer<std::vector<std::vector<double>>> rawContainer(
       finalHMap);
-  rawContainer.showRawMap();
-  saveJSON(finalHMap, "heightmap.json");
+  HeightMapFinContainer<std::vector<std::vector<double>>> finContainer(
+      rawContainer.getValue());
+  PostProcessor postProc{finContainer};
+  postProc.normalizeFunction();
+  postProc.remapFunction(coefficient);
+  postProc.scalingFunction(terrainMainConfig.minHeight,
+                           terrainMainConfig.maxHeight);
+  finContainer.showRawMap();
+  finContainer.showMin();
+  finContainer.showMax();
+  finContainer.showMid(std::pow(nodes, 2));
+  saveJSON(finContainer.getValue(), "heightmap.json");
 }
