1e82a0a7dce96a715236fe1ccb815a5f77aecb22
lautrec93 <lautrec.willson@gmail.com>
2026-03-20 20:59:36 +0100

    chore: create constructor for a structure

--- src/generator/TerrainConfig.hpp
+++ src/generator/TerrainConfig.hpp
@@ -17,4 +17,12 @@ struct TerrainConfig {
   float maxHeight{};
   float minHeight{};
   float seaLevel{};
+
+  TerrainConfig(uint32_t seed, unsigned size, float cellSize, float noizeScale,
+                unsigned octaveNumber, float lacunarity, float persistence,
+                float maxHeight, float minHeight, float seaLevel)
+      : seed(seed), size(size), cellSize(cellSize), noizeScale(noizeScale),
+        octaveNumber(octaveNumber), lacunarity(lacunarity),
+        persistence(persistence), minHeight(minHeight), maxHeight(maxHeight),
+        seaLevel(seaLevel) {}
 };
