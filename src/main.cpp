d7996c1b0a56c2525fa89e494f954b3b612d6d0b
lautrec93 <lautrec.willson@gmail.com>
2026-04-04 18:00:54 +0200

    chore: change some directories. feat: add new class initialisatoins

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,32 @@
 #include "generator/FractalBrownianMotion.hpp"
+#include "generator/Globals.hpp"
 #include "generator/HeightMapContainer.hpp"
 #include "generator/HeightMapGenerator.hpp"
 #include "generator/Input.hpp"
+#include "generator/MeshF/CoordsBuilder.hpp"
+#include "generator/MeshF/Mesh.hpp"
+#include "generator/MeshF/MeshBuilder.hpp"
+#include "generator/MeshF/NormalBuilder.hpp"
+#include "generator/MeshF/SingleAngleNormalHandler.hpp"
 #include "generator/NoiseParametres.hpp"
 #include "generator/PerlinNoise.hpp"
 #include "generator/PostProcessor.hpp"
 #include "generator/SaveJSON.hpp"
+#include "generator/TerrainConfig.hpp"
 #include <cctype>
 #include <string>
 #include <vector>
+int example();
 int main() {
 
   Input input;
   auto inputValues = input.startInput();
 
-  unsigned nodes = inputValues[0].size / inputValues[0].cellSize;
+  NUMBER_OF_NODES_IN_LINE = inputValues[0].size / inputValues[0].cellSize;
 
   HeightMapGenerator heightMainMapGenerator{inputValues[0]};
   HeightMapContainer heightMapMainContainer{
-      heightMainMapGenerator.fieldXYCreator(nodes)};
+      heightMainMapGenerator.fieldXYCreator(NUMBER_OF_NODES_IN_LINE)};
 
   NoiseParametres perlinNoiseParam{heightMapMainContainer.getHeightMap(),
                                    inputValues[0]};
@@ -35,6 +43,15 @@ int main() {
   // heightMapMainContainer.showMap();
   heightMapMainContainer.showMin();
   heightMapMainContainer.showMax();
-  heightMapMainContainer.showMid(std::pow(nodes, 2));
+  heightMapMainContainer.showMid(std::pow(NUMBER_OF_NODES_IN_LINE, 2));
   saveJSON(heightMapMainContainer.getHeightMap(), "heightmap.json");
+  CoordsBuilder coordsBuilder(heightMapMainContainer, inputValues[0]);
+  Mesh mesh;
+  NeighbouredTrianglesFinder nTF(mesh, inputValues[0]);
+  SingleAngleNormalHandler sANH;
+  NormalBuilder normalBuilder(mesh, inputValues[0], coordsBuilder, nTF, sANH);
+  UVBuilder uvBuilder(inputValues[0]);
+  VerticesBuilder verticesBuilder(coordsBuilder, normalBuilder, uvBuilder);
+  MeshBuilder meshBuilder(mesh, verticesBuilder);
+  meshBuilder.meshBuild();
 }
