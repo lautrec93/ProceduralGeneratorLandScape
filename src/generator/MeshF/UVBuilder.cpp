5ef2e38adc00d52ffe8190fbbb62cf42365bef63
lautrec93 <lautrec.willson@gmail.com>
2026-04-09 01:07:55 +0200

    chore: delete extra variable

--- src/generator/MeshF/UVBuilder.cpp
+++ src/generator/MeshF/UVBuilder.cpp
@@ -1,10 +1,8 @@
 #include "generator/MeshF/UVBuilder.hpp"
 #include "generator/Globals.hpp"
 UV UVBuilder::uvCounter(unsigned index) {
-  uv.u = ((index / NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize) /
-         (5 * terrainConfig.cellSize);
-  uv.v = ((index % NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize) /
-         (5 * terrainConfig.cellSize);
+  uv.u = ((index / NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize) / 10;
+  uv.v = ((index % NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize) / 10;
 
   return uv;
 }
