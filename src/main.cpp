b2597dfecb14ce20b812d6fd450cedf971e0005f
lautrec93 <lautrec.willson@gmail.com>
2026-04-16 16:10:25 +0200

    feat: main update

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "glad/glad.h"
+
+#include "GLFW/glfw3.h"
 #include "generator/HeightMap/HeightMapBuilder.hpp"
 #include "generator/HeightMap/HeightMapContainer.hpp"
 #include "generator/IOConfigs/Input.hpp"
@@ -13,12 +16,46 @@
 #include "generator/Noise/PerlinNoise.hpp"
 #include "generator/Noise/PostProcessor.hpp"
 #include <cctype>
+#include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main() {
 
-  Input input;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(1920, 1080, "TerrainGen", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create the window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to connect GLAD" << std::endl;
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glClearColor(0.3f, 0.4f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  /*Input input;
   auto inputValues = input.startInput();
 
   NUMBER_OF_NODES_IN_LINE = inputValues[0].size / inputValues[0].cellSize;
@@ -35,13 +72,26 @@ int main() {
   heightMapMainContainer.showMid();
   saveJSON(heightMapMainContainer.getHeightMap(), "heightmap.json");
 
-  /*CoordsBuilder coordsBuilder(heightMapMainContainer, inputValues[0]);
+  CoordsBuilder coordsBuilder(heightMapMainContainer, inputValues[0]);
   Mesh mesh;
   SingleAngleNormalHandler sANH;
   NormalBuilder normalBuilder(mesh, coordsBuilder, sANH);
   UVBuilder uvBuilder(inputValues[0]);
+  ColourBuilder colourBuilder(perlinNoise);
   VerticesBuilder verticesBuilder(inputValues[0], coordsBuilder, normalBuilder,
-                                  uvBuilder);
+                                  uvBuilder, colourBuilder);
   MeshBuilder meshBuilder(mesh, verticesBuilder);
   meshBuilder.meshBuild();*/
+  glfwTerminate();
+  return 0;
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
