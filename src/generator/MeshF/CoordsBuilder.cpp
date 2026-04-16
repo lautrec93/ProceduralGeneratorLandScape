11618ea093622b8c94bd6886a7b2a8b1add9d162
lautrec93 <lautrec.willson@gmail.com>
2026-04-16 16:06:38 +0200

    chore: delete helper-lines

--- src/generator/MeshF/CoordsBuilder.cpp
+++ src/generator/MeshF/CoordsBuilder.cpp
@@ -1,11 +1,8 @@
 #include "generator/MeshF/CoordsBuilder.hpp"
 #include "generator/Instruments/Globals.hpp"
-#include <iostream>
 Coord CoordsBuilder::coordsCounting(unsigned index) {
   coord.x = (index / NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize;
   coord.z = (index % NUMBER_OF_NODES_IN_LINE) * terrainConfig.cellSize;
-  std::cout << "size = " << heightMapContainer.getHeightMap().size() << '\n';
-  std::cout << "index = " << index << '\n';
   coord.y = heightMapContainer.getHeightMap()[index];
 
   return coord;
